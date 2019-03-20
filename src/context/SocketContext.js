dcae00e087f1576b14de8543edaf51bb8d5416c0
abbabara <arnbjorgf17@ru.is>
2019-03-20 20:28:25 +0000

    m

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -1,8 +1,3 @@
-import {createContext} from 'react';
 import connectToSocketIoServer from 'socket.io-client';
 
-const SocketContext = createContext({
-    socket: connectToSocketIoServer('http://localhost:8080')
-});
-
-export default SocketContext;
+export default socket = connectToSocketIoServer("http://localhost/8080");
